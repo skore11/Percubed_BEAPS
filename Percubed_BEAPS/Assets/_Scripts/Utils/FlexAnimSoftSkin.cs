906baa26e5681daa1a9fb141b334815c024df7b7
Stefan Rank <strank@strank.info>
2021-03-18 12:03:38 -0400

    Fixing AnimTracking, using NonHumanoid scene, some debugging prints left in for n ow. Still missing: factor in rotation of imported mesh. Also temporarily set flexcontainer to low gravity with a bit of friction to ease testing. Somewhat tracks the animation now if no particles are locked after moving to the mesh rotation spot.

--- Percubed_BEAPS/Assets/_Scripts/Utils/FlexAnimSoftSkin.cs
+++ Percubed_BEAPS/Assets/_Scripts/Utils/FlexAnimSoftSkin.cs
@@ -36,35 +36,32 @@ namespace Percubed.Flex
     [RequireComponent(typeof(FlexSoftActor))]
     public class FlexAnimSoftSkin : MonoBehaviour
     {
-        public SkinnedMeshRenderer referenceAnimSkin; // hmmmmmm this cannot be the local skinnedmeshrenderer,
-        // as that one is used by FlexSoftSkinning and its bones are set by that!
-        // SHOULD be using the original animation all along anyway! also in the other setup probably!
-        // i.e. the default avatar playing the animation (potentially offscreen)
+        public SkinnedMeshRenderer referenceAnimSkin; // the original animation,
+        // i.e. the original model playing the animation (potentially offscreen)
 
         private FlexSoftActor m_actor;
-        // caches for values copied in and out of the flex subsystem:
+        /// caches for values copied in and out of the flex subsystem:
         private Vector4[] m_particles;
         private Vector3[] m_velocities;
 
-        // Performance note: all of the following could be arrays instead of Lists for a small performance boost
+        /// Particle to Vertex / Bones relations that are calculated once on first run:
 
-        // index of the mesh vertices that are closest to each Flex particle in the rest state
-        // the bone weights of that vertex will be used to animate that particle:
-        private List<int> nearestVertexIndexForParticle = new List<int>();
-        // The offset fro the nearest Vertex to "its" particle
+        // The offset from the nearest Vertex to "its" particle
         // (Note: we might not need that, if we change ParticleBoneWeight to just store the offset to the
-        // particle rather than to the vertex?)
-        private List<Vector3> VertOffsetVectors = new List<Vector3>();
-        // local copy of the world position for all Flex particles
-        private List<Vector3> particlePositions = new List<Vector3>();
-        // Needed to update the mass particle positions after assigning bone weights from vertex mapping
-        private List<Vector3> particleRestPositions = new List<Vector3>();
+        // particle rather than to the vertex? Also could be an array instead of a list for small performance boost)
+        private List<Vector3> vertOffsetVectors = new List<Vector3>();
         // for each bone of the original mesh, store a list of weights for the particles it should influence:
-        public WeightList[] particleBoneWeights;
+        public WeightList[] particleBoneWeights; // (only public for DEBUG)
 
-        private Vector3[] particleDisplacementVector; // used when updating particle positions
+        /// Used when updating particle positions (in local space) and calculating velocities:
+        private Vector3[] particlePositions;
+        private Vector3[] particleDisplacements;
 
         private bool firstRun = true;
+
+        /// DEBUG:
+        private bool debugCollect = false;
+        private string debugStr = "";
 
         private void Awake()
         {
@@ -81,8 +78,8 @@ namespace Percubed.Flex
             // local caches used during the update of particle positions based on the reference animation:
             m_particles = new Vector4[m_actor.indexCount];
             m_velocities = new Vector3[m_particles.Length];
-            // TODO: rename this one:
-            particleDisplacementVector = new Vector3[m_particles.Length];
+            particleDisplacements = new Vector3[m_particles.Length];
+            particlePositions = new Vector3[m_particles.Length];
             string debugOutput = "Animating " + m_actor.name + " based on Skin " + referenceAnimSkin.name;
             debugOutput += "\n Indices: " + m_actor.indexCount + " from " + m_actor.indices[0] + " to " + m_actor.indices[m_actor.indexCount - 1];
             debugOutput += "\n mass Scale: " + m_actor.massScale;
@@ -91,29 +88,51 @@ namespace Percubed.Flex
             debugOutput += "\n shapeindices length: " + m_actor.asset.shapeCenters.Length;
             debugOutput += "\n fixed particles: " + m_actor.asset.fixedParticles.Length;
             Debug.Log(debugOutput);
+            if (m_actor.asset.referenceShape != -1)
+            {
+                Debug.Log("Unsetting reference shape for " + m_actor.name + " to avoid FlexSoftActor automatically moving the transform" +
+                        " as this would interfere with tracking the animation.");
+                this.m_actor.asset.referenceShape = -1;
+            }
+            StartCoroutine(DebugDump());
+        }
+
+        IEnumerator DebugDump()
+        {
+            yield return null;
+            while (Application.isPlaying)
+            {
+                debugStr = "";
+                debugCollect = true;
+                while (debugStr == "")
+                {
+                    yield return null;
+                }
+                debugCollect = false;
+                Debug.Log("DebugDump for FlexAnimSoftSkin of " + m_actor.name + debugStr);
+                yield return new WaitForSeconds(0.5f);
+            }
         }
 
         void OnFlexUpdate(FlexContainer.ParticleData _particleData)
         {
-            // Fill a local copy of the particles:
+            // Fill a local copy of the particles: NOTE: the particle coordinates are in world space
             _particleData.GetParticles(m_actor.indices[0], m_actor.indexCount, m_particles);
             if (firstRun)
             {
                 firstRun = false; // only run this once:
-                Debug.Log("Creating Mapping from Particles to Bones based on closest Vertex of the original mesh!");                
-                // TODO: re-check full algorithm below...
+                // The mesh vertices are in local space:
                 Vector3[] cachedMeshVertices = referenceAnimSkin.sharedMesh.vertices;
+                // list of the index of the mesh vertex that is closest to each Flex particle in the rest state
+                // the bone weights of that vertex will be used to animate that particle:
+                List<int> nearestVertexIndexForParticle = new List<int>();
+                // Use the particle position transformed to local space to setup the needed mappings:
                 foreach (var i in m_particles)
                 {
-                    Vector3 restPos = new Vector3();
-                    restPos.x = i.x;
-                    restPos.y = i.y;
-                    restPos.z = i.z;
-                    particleRestPositions.Add(restPos);
-                    particlePositions.Add(restPos);
-                    int nearestIndexforParticle = GetNearestVertIndex(restPos, cachedMeshVertices);
-                    Vector3 VertOffset = restPos - cachedMeshVertices[nearestIndexforParticle];
-                    VertOffsetVectors.Add(VertOffset);
+                    Vector3 localPos = this.transform.InverseTransformPoint(i);
+                    int nearestIndexforParticle = GetNearestVertIndex(localPos, cachedMeshVertices);
+                    Vector3 VertOffset = localPos - cachedMeshVertices[nearestIndexforParticle];
+                    vertOffsetVectors.Add(VertOffset);
                     nearestVertexIndexForParticle.Add(nearestIndexforParticle);
                 }
                 SetBoneWeights(cachedMeshVertices, nearestVertexIndexForParticle);
@@ -126,8 +145,14 @@ namespace Percubed.Flex
 
         public void UpdateParticlePositions(FlexContainer.ParticleData _particleData)
         {
+            // In the following, m_particles and m_velocities always work in world space,
+            // while the calculation for particlePositions are done based on the bindposes and the bones of
+            // the reference animation skin (which is in a different location in the world)
+            // so they need to then be moved to the world position of this gameobject
+            // before calculating the particleDisplacements in world space again so it can be applied to the particles!
+
             // "Zero out" all particle postions first
-            for (int i = 0; i < particlePositions.Count; i++)
+            for (int i = 0; i < particlePositions.Length; i++)
             {
                 particlePositions[i] = Vector3.zero;
             }
@@ -136,47 +161,51 @@ namespace Percubed.Flex
             {
                 foreach (ParticleBoneWeight pbw in wList.weights)
                 {
-                    // use the current transform of the bone to add its contribution to the new particle position:
+                    // Use the current transform of the bone to add its contribution to the new particle position:
                     Transform t = referenceAnimSkin.bones[wList.boneIndex];
-                    // Adding the VertOffsetVector moves it from the original vertex to the particle
-                    particlePositions[pbw.particleIndex] += t.localToWorldMatrix.MultiplyPoint3x4(pbw.localPosition) * pbw.weight + VertOffsetVectors[pbw.particleIndex];
+                    // It contributes its full hierarchy transform, i.e. what it considers local space to world space,
+                    // moving the vertex of the mesh to where it should be in world space for the reference animation right now.
+                    // Adding the VertOffsetVector gives us the location of the particle according to this bone right now
+                    particlePositions[pbw.particleIndex] += pbw.weight * (t.TransformPoint(pbw.localPosition) + vertOffsetVectors[pbw.particleIndex]);
                 }
             }
-            // We now have the particlePositions as they should be based on the animation,
+            // We now have the particlePositions as they should be based on the animation, in world space,
             // BUT at the position of the reference animation's root bone in world space.
-            // First move it back to local space relative to its root bone.
-            // And then we should have to move it back to world space relative to this GameObject,
-            // but for some reason that is not needed - but it breaks when this GameObject is rotated
-            // (didn't test scaling). There must be something in the Flex setup that moves the points? :
-            for (int i = 0; i < particlePositions.Count; i++)
+            Vector3 _tempVector3Local;
+            Vector3 _tempVector3Global; // (split into three different temps for DEBUG)
+            Vector3 _tempVector3Diff;
+            for (int i = 0; i < particlePositions.Length; i++)
             {
-                particlePositions[i] = referenceAnimSkin.rootBone.transform.InverseTransformPoint(particlePositions[i]);
-                //particlePositions[i] = this.transform.TransformPoint(particlePositions[i]);
-            }
-            // Now we should have the target particlePositions
-            if (particlePositions.Count == 0) // this check shouldn't be needed now without uFlex, but leaving it in
-            {
-                return;
-            }
-            // Now for each particle, calculate how far it is from its intended (animation-)position:
-            Vector3 _tempVector3;
-            for (int particleIndex = 0; particleIndex < m_particles.Length; particleIndex++)
-            {
-                // casting a Vector4 to a Vector3 automatically discards the w component:
-                _tempVector3 = particlePositions[particleIndex] - ((Vector3) m_particles[particleIndex]);
-                particleDisplacementVector[particleIndex].x = _tempVector3.x;
-                particleDisplacementVector[particleIndex].y = _tempVector3.y;
-                particleDisplacementVector[particleIndex].z = _tempVector3.z;
+                // Now: First move it back to local space relative to its root bone.
+                _tempVector3Local = referenceAnimSkin.rootBone.InverseTransformPoint(particlePositions[i]);
+                // And then move it back to world space relative to this GameObject
+                // and calculate how far it is from its intended (animation-)position:
+                // (casting a Vector4 to a Vector3 automatically discards the w component)
+                _tempVector3Global = this.transform.TransformPoint(_tempVector3Local);
+                _tempVector3Diff = _tempVector3Global - ((Vector3) m_particles[i]);
+                particleDisplacements[i].x = _tempVector3Diff.x;
+                particleDisplacements[i].y = _tempVector3Diff.y;
+                particleDisplacements[i].z = _tempVector3Diff.z;
+                // DEBUG:
+                if (debugCollect)
+                {
+                    if (i % 1000 == 0)
+                    {
+                        debugStr += string.Format("\nparticle {0}  \treferenceLocalPos {1}  \tglobalPos {2}  \tparticlePos {3}  \tdiff {4}",
+                                i, _tempVector3Local, _tempVector3Global, m_particles[i], _tempVector3Diff);
+                    }
+                }
             }
             // Now apply an appropriate velocity change to nudge the particle into the right direction:
             // Get/Set all velocities at the same time, as this is the most performant method:
             _particleData.GetVelocities(m_actor.indices[0], m_actor.indexCount, m_velocities);
-            for (int i = 0; i < particleDisplacementVector.Length; i++)
+            for (int i = 0; i < particleDisplacements.Length; i++)
             {
                 // Note: we replicate roughly what ApplyImpulses in Actor would do, i.e. scale by weight:
                 // impulse divided by particle mass (which is 1/w):
                 //m_actor.ApplyImpulse(particleDisplacementVector[i], i);
-                m_velocities[i] += particleDisplacementVector[i] * m_particles[i].w ;
+                m_velocities[i] += particleDisplacements[i] * m_particles[i].w * 0.1f ; // DEBUG: temp. small factor for clarity
+                // TODO: find physically appropriate factor!
             }
             _particleData.SetVelocities(m_actor.indices[0], m_actor.indexCount, m_velocities);
         }
@@ -189,8 +218,7 @@ namespace Percubed.Flex
             WeightList[] boneWeights = new WeightList[referenceAnimSkin.bones.Length];
             for (int i = 0; i < referenceAnimSkin.bones.Length; i++)
             {
-                boneWeights[i] = new WeightList();
-                boneWeights[i].boneIndex = i;
+                boneWeights[i] = new WeightList { boneIndex = i };
             }
             // for every particle, add up to 4 appropriate VertexWeights based on the bones affecting the nearest vertex
             for (int particleIndex = 0; particleIndex < nearestVertexIndexForParticle.Count; particleIndex++)
@@ -198,7 +226,8 @@ namespace Percubed.Flex
                 int nearestVertexIndex = nearestVertexIndexForParticle[particleIndex];
                 Vector3 vertexPos = cachedMeshVertices[nearestVertexIndex];
                 // for each non-zero BoneWeight of that vertex, add a corresponding VertexWeight,
-                // remembering the offset from the bone to the vertex (so it can be used on the particle later)
+                // remembering the offset from the bone to the vertex in bone-local space
+                // i.e. after multiplying with the corresponding bindpose (so it can be used on the particle later)
                 BoneWeight bw = cachedBoneWeights[nearestVertexIndex];
                 if (bw.weight0 != 0.0f)
                 {
@@ -219,6 +248,29 @@ namespace Percubed.Flex
                 {
                     Vector3 localPt = cachedBindposes[bw.boneIndex3].MultiplyPoint3x4(vertexPos);
                     boneWeights[bw.boneIndex3].weights.Add(new ParticleBoneWeight(particleIndex, localPt, bw.weight3));
+                }
+                // DEBUG:
+                if (particleIndex % 1000 == 0)
+                {
+                    var debugPBW = "";
+                    for (int i = 0; i < referenceAnimSkin.bones.Length; i++)
+                    {
+                        foreach (ParticleBoneWeight pbw in boneWeights[i].weights) {
+                            if (pbw.particleIndex == particleIndex)
+                            {
+                                debugPBW += string.Format("bone {0} localPos {1} weight {2} ",
+                                        i, pbw.localPosition, pbw.weight);
+                            }
+                        }
+                    }
+                    Debug.Log(string.Format("particle {0} nearest vertex {1} at {2}" +
+                            "\nparticle world pos {3} local pos {4}" +
+                            "\nanimation bone weight {5}\nparticle bone weights {6}" +
+                            "\nreference anim skin bones count {7} bindposes {8} boneweights {9}",
+                            particleIndex, nearestVertexIndex, vertexPos,
+                            m_particles[particleIndex], this.transform.InverseTransformPoint(m_particles[particleIndex]),
+                            JsonUtility.ToJson(bw), debugPBW,
+                            referenceAnimSkin.bones.Length, cachedBindposes.Length, cachedBoneWeights.Length));
                 }
             }
             particleBoneWeights = boneWeights;
